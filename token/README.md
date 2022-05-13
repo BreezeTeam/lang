76417b44c534bd25cd682385dd4cbc6982f15cdf
euraxluo <1793939176@qq.com>
2022-05-13 08:18:46 +0800

    token done lexer writing sup rune :TODO

---
+++ token/README.md
@@ -0,0 +1,9 @@
+# Token
+
+```javascript
+let five = 5;
+let ten = 10;
+let add = func(x, y) { x + y;
+};
+let result = add(five, ten);
+```
---
+++ token/token.go
@@ -0,0 +1,70 @@
+package token
+
+type TokenType string
+
+// 符号表
+const (
+
+	// ILLEGAL 非法字符
+	ILLEGAL = "ILLEGAL"
+	// EOF 文件结尾
+	EOF = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"func":   FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
