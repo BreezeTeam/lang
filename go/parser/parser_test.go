abc37c2612c132a37fea2f2671436246634c2018
euraxluo <1793939176@qq.com>
2022-05-22 18:30:56 +0800

    add Parsing todo: while,func,call

--- go/parser/parser_test.go
+++ go/parser/parser_test.go
@@ -5,12 +5,7 @@ import (
 	"testing"
 )
 
-//
-//  TestParser_ProgramParser
-//  @Description: 正确的测试方法是构建 词法解析器，通过确定性的结果进行语法分析器测试
-//  @param t
-//
-func TestParser_ProgramParser(t *testing.T) {
+func TestLetStatements(t *testing.T) {
 	tests := []struct {
 		input         string
 		wantStatement int
@@ -36,7 +31,11 @@ func TestParser_ProgramParser(t *testing.T) {
 			1,
 		},
 		{
-			"let x=1;",
+			"let x=12DSADAS;",
+			1,
+		},
+		{
+			"let x=y;",
 			1,
 		},
 		{
@@ -46,14 +45,447 @@ func TestParser_ProgramParser(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.input, func(t *testing.T) {
-			programe := NewParser(lexer.NewLexer(tt.input)).ProgramParser()
-			if programe == nil {
+			p := NewParser(lexer.NewLexer(tt.input))
+			program := p.ProgramParser()
+			checkParserErrors(t, p)
+			if program == nil {
 				t.Errorf("`%s` parser result is nil", tt.input)
 			} else {
-				if len(programe.Statement) != tt.wantStatement {
-					t.Errorf("`%s` parser Statement is %d ,want %d", tt.input, len(programe.Statement), tt.wantStatement)
+				t.Log(program.String())
+				if len(program.Statement) != tt.wantStatement {
+					t.Errorf("`%s` parser Statement is %d ,want %d", tt.input, len(program.Statement), tt.wantStatement)
 				}
 			}
 		})
 	}
+}
+func TestReturnStatements(t *testing.T) {
+	tests := []struct {
+		input         string
+		wantStatement int
+	}{
+		{
+			"return",
+			0,
+		},
+		{
+			"return 0",
+			0,
+		},
+		{
+			"return asa",
+			0,
+		},
+		{
+			"return as;",
+			1,
+		},
+		{
+			"return;",
+			0,
+		},
+		{
+			"return abc(1231)",
+			0,
+		},
+		{
+			"return abc(1231);",
+			1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			p := NewParser(lexer.NewLexer(tt.input))
+			program := p.ProgramParser()
+			checkParserErrors(t, p)
+			if program == nil {
+				t.Errorf("`%s` parser result is nil", tt.input)
+			} else {
+				t.Log(program.String())
+				if len(program.Statement) != tt.wantStatement {
+					t.Errorf("`%s` parser Statement is %d ,want %d", tt.input, len(program.Statement), tt.wantStatement)
+				}
+			}
+		})
+	}
+}
+
+func TestIdentifierExpression(t *testing.T) {
+	tests := []struct {
+		input         string
+		wantStatement int
+	}{
+		{
+			"abc",
+			1,
+		},
+		{
+			"abc;",
+			1,
+		},
+		{
+			"123abc;",
+			1,
+		},
+		{
+			"abc123;",
+			1,
+		},
+		{
+			"_abc123;",
+			1,
+		},
+		{
+			"_abc中文123;",
+			1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			p := NewParser(lexer.NewLexer(tt.input))
+			program := p.ProgramParser()
+			checkParserErrors(t, p)
+			if program == nil {
+				t.Errorf("`%s` parser result is nil", tt.input)
+			} else {
+				t.Log(program.String())
+				if len(program.Statement) != tt.wantStatement {
+					t.Errorf("`%s` parser Statement is %d ,want %d", tt.input, len(program.Statement), tt.wantStatement)
+				}
+			}
+		})
+	}
+}
+
+func TestIntegerLiteralExpression(t *testing.T) {
+	tests := []struct {
+		input         string
+		wantStatement int
+	}{
+		{
+			"123",
+			1,
+		},
+		{
+			"123abc;",
+			0,
+		},
+		{
+			"123;",
+			1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			p := NewParser(lexer.NewLexer(tt.input))
+			program := p.ProgramParser()
+			checkParserErrors(t, p)
+			if program == nil {
+				t.Errorf("`%s` parser result is nil", tt.input)
+			} else {
+				t.Log(program.String())
+				if len(program.Statement) != tt.wantStatement {
+					t.Errorf("`%s` parser Statement is %d ,want %d", tt.input, len(program.Statement), tt.wantStatement)
+				}
+			}
+		})
+	}
+}
+
+func TestParsingPrefixExpressions(t *testing.T) {
+	tests := []struct {
+		input         string
+		wantStatement int
+	}{
+		{
+			"!5",
+			1,
+		},
+		{
+			"-5;",
+			1,
+		},
+		{
+			"+5;",
+			1,
+		},
+		{
+			"!dsa;",
+			1,
+		},
+		{
+			"-dsa;",
+			1,
+		},
+		{
+			"!true;",
+			1,
+		},
+		{
+			"!_sa中文;",
+			1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			p := NewParser(lexer.NewLexer(tt.input))
+			program := p.ProgramParser()
+			checkParserErrors(t, p)
+			if program == nil {
+				t.Errorf("`%s` parser result is nil", tt.input)
+			} else {
+				t.Log(program.String())
+				if len(program.Statement) != tt.wantStatement {
+					t.Errorf("`%s` parser Statement is %d ,want %d", tt.input, len(program.Statement), tt.wantStatement)
+				}
+			}
+		})
+	}
+}
+
+func TestParsingInfixExpressions(t *testing.T) {
+	tests := []struct {
+		input         string
+		left          int
+		operator      string
+		right         int
+		wantStatement int
+	}{
+		{"5 + 5;", 5, "+", 5, 1},
+		{"5 - 5;", 5, "-", 5, 1},
+		{"5 * 5;", 5, "*", 5, 1},
+		{"5 / 5;", 5, "/", 5, 1},
+		{"5 > 5;", 5, ">", 5, 1},
+		{"5 < 5;", 5, "<", 5, 1},
+		{"5 == 5;", 5, "==", 5, 1},
+		{"5 != 5;", 5, "!=", 5, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			p := NewParser(lexer.NewLexer(tt.input))
+			program := p.ProgramParser()
+			checkParserErrors(t, p)
+			if program == nil {
+				t.Errorf("`%s` parser result is nil", tt.input)
+			} else {
+				t.Log(program.String())
+				if len(program.Statement) != tt.wantStatement {
+					t.Errorf("`%s` parser Statement is %d ,want %d", tt.input, len(program.Statement), tt.wantStatement)
+				}
+			}
+		})
+	}
+}
+
+func TestParsingBooleanExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			"-a * b",
+			"((-a) * b)",
+		},
+		{
+			"!-a",
+			"(!(-a))",
+		},
+		{
+			"a + b + c",
+			"((a + b) + c)",
+		},
+		{
+			"a + b - c",
+			"((a + b) - c)",
+		},
+		{
+			"a * b * c",
+			"((a * b) * c)",
+		},
+		{
+			"a * b / c",
+			"((a * b) / c)",
+		},
+		{
+			"a + b / c",
+			"(a + (b / c))",
+		},
+		{
+			"a + b * c + d / e - f",
+			"(((a + (b * c)) + (d / e)) - f)",
+		},
+		{
+			"3 + 4; -5 * 5",
+			"(3 + 4)((-5) * 5)",
+		},
+		{
+			"5 > 4 == 3 < 4",
+			"((5 > 4) == (3 < 4))",
+		},
+		{
+			"5 < 4 != 3 > 4",
+			"((5 < 4) != (3 > 4))",
+		},
+		{
+			"3 + 4 * 5 == 3 * 1 + +4 * 5",
+			"((3 + (4 * 5)) == ((3 * 1) + ((+4) * 5)))",
+		},
+		{
+			"3 + 4 * 5 == 3 * 1 + 4 * 5",
+			"((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
+		},
+		{
+			"let x = 1 * 2 + 1 == 1 + 1 * 2",
+			"let x = (((1 * 2) + 1) == (1 + (1 * 2)));",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			p := NewParser(lexer.NewLexer(tt.input))
+			program := p.ProgramParser()
+			checkParserErrors(t, p)
+			if program == nil {
+				t.Errorf("`%s` parser result is nil", tt.input)
+			} else {
+				if program.String() != tt.expected {
+					t.Errorf("`%s` parser Statement is %s ,want %s", tt.input, program.String(), tt.expected)
+				} else {
+					t.Logf("`%s` => `%s`", tt.input, program.String())
+				}
+			}
+		})
+	}
+}
+func TestOperatorPrecedenceParsing(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			"true",
+			"true",
+		},
+		{
+			"false",
+			"false",
+		},
+		{
+			"3 > 5 == false",
+			"((3 > 5) == false)",
+		},
+		{
+			"3 < 5 == true",
+			"((3 < 5) == true)",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			p := NewParser(lexer.NewLexer(tt.input))
+			program := p.ProgramParser()
+			checkParserErrors(t, p)
+			if program == nil {
+				t.Errorf("`%s` parser result is nil", tt.input)
+			} else {
+				if program.String() != tt.expected {
+					t.Errorf("`%s` parser Statement is %s ,want %s", tt.input, program.String(), tt.expected)
+				} else {
+					t.Logf("`%s` => `%s`", tt.input, program.String())
+				}
+			}
+		})
+	}
+}
+
+func TestOperatorPrecedenceWithGroupedParsing(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			"1 + (2 + 3) + 4",
+			"((1 + (2 + 3)) + 4)",
+		},
+		{
+			"(a)",
+			"a",
+		},
+		{
+			"(5 + 5) * 2",
+			"((5 + 5) * 2)",
+		},
+		{
+			"2 / (5 + 5)",
+			"(2 / (5 + 5))",
+		},
+		{
+			"-(5 + 5)",
+			"(-(5 + 5))",
+		},
+		{
+			"!(true == true)",
+			"(!(true == true))",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			p := NewParser(lexer.NewLexer(tt.input))
+			program := p.ProgramParser()
+			checkParserErrors(t, p)
+			if program == nil {
+				t.Errorf("`%s` parser result is nil", tt.input)
+			} else {
+				if program.String() != tt.expected {
+					t.Errorf("`%s` parser Statement is %s ,want %s", tt.input, program.String(), tt.expected)
+				} else {
+					t.Logf("`%s` => `%s`", tt.input, program.String())
+				}
+			}
+		})
+	}
+}
+
+func TestParseIfExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			"if (x<5){x}else{y}",
+			"if (x < 5) { x } else { y } ",
+		},
+		{
+			"if x<5{x}else{y}",
+			"if (x < 5) { x } else { y } ",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			p := NewParser(lexer.NewLexer(tt.input))
+			program := p.ProgramParser()
+			checkParserErrors(t, p)
+			if program == nil {
+				t.Errorf("`%s` parser result is nil", tt.input)
+			} else {
+				if program.String() != tt.expected {
+					t.Errorf("`%s` parser Statement is %s ,want %s", tt.input, program.String(), tt.expected)
+				} else {
+					t.Logf("`%s` => `%s`", tt.input, program.String())
+				}
+			}
+		})
+	}
+}
+
+// checkParserErrors  验证有没有解析错误
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
